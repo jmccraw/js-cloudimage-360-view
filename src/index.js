a68d0c9f5ae95bafe3ffb56d7ce310536c0fe56f
Justin McCraw <justin.mccraw@espn.com>
2020-02-07 14:39:29 -0500

    Extend public functions so we can lazy load in some viewers

--- src/index.js
+++ src/index.js
@@ -13,6 +13,34 @@ function init() {
   window.CI360._viewers = viewers;
 }
 
+/**
+ * Lazy-load allowance so a new viewer can be added to the scene
+ * @param {Object} _newViewer The new viewer to load into the scene
+ */
+function loadNewViewer( _newViewer, _newInstructions ) {
+  window.CI360._viewers.push( new CI360Viewer( _newViewer ) );
+}
+
+/**
+ * Pauses the current viewer
+ * @param {Number} index The viewer index
+ */
+function pauseViewer( index ) {
+  if ( window.CI360._viewers[index] ) {
+    window.CI360._viewers[index].isPlaying = false;
+  }
+}
+
+/**
+ * Plays the current viewer
+ * @param {Number} index The viewer index
+ */
+function playViewer( index ) {
+  if ( window.CI360._viewers[index] ) {
+    window.CI360._viewers[index].isPlaying = true;
+  }
+}
+
 function destroy() {
   if (isNoViewers()) return;
 
@@ -37,6 +65,9 @@ window.CI360 = window.CI360 || {};
 window.CI360.init = init;
 window.CI360.destroy = destroy;
 window.CI360.getActiveIndexByID = getActiveIndexByID;
+window.CI360.loadNewViewer = loadNewViewer;
+window.CI360.pauseViewer = pauseViewer;
+window.CI360.playViewer = playViewer;
 
 if (!window.CI360.notInitOnLoad) {
   init();
